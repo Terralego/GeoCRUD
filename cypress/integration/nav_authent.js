408c8bac235426bc3c5cd454dd1b83ea3fc6d249
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2020-06-18 17:13:04 +0200

    Add tests

--- cypress/integration/nav_authent.js
+++ cypress/integration/nav_authent.js
@@ -1,3 +1,39 @@
+describe('Login from home page', () => {
+  it('Redirects to login page.', () => {
+    cy.visit('http://127.0.0.1:3000')
+    cy.get('form')
+    cy.contains("Email")
+    cy.get('input[type="email"]')
+    cy.get('input[type="password"]')
+    })
+
+  it('Fail to login', () => {
+    cy.visit('http://127.0.0.1:3000')
+    cy.get('input[type="email"]')
+      .type('fake@fake.fake')
+      .should('have.value', 'fake@fake.fake')
+    cy.get('input[type="password"]')
+      .type('password')
+      .should('have.value', 'password')
+    cy.get("button[type='submit']").click()
+    cy.get('[icon="error"]')
+  })
+
+  it('Login', () => {
+    cy.visit('http://127.0.0.1:3000/')
+    cy.get('[type="email"]')
+      .type('admin@admin.admin')
+      .should('have.value', 'admin@admin.admin')
+    cy.get('[type="password"]')
+      .type('admin')
+      .should('have.value', 'admin')
+    cy.get("button[type='submit']").click()
+    cy.url().should('include', '/CRUD/map/cities')
+    cy.get("span.bp3-button-text").contains("admin@admin.admin")
+    localStorage.getItem('tf:auth:token')
+  })
+})
+
 describe('Logout', () => {
     beforeEach(() => {
       const email = 'admin@admin.admin'
