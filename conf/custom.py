a64fa69c3b96e4329e0c74f788e491af05ddd95f
J-E Castagnede <j.e.castagnede@gmail.com>
2020-10-06 15:30:46 +0200

    fix conf files

--- conf/custom.py
+++
@@ -1,7 +0,0 @@
-from .prod import *  #NOQA
-
-# Write or edit all django settings values available
-# Add dependencies, custom apps, environment variable
-ADMINS = (
-    # ('example', "test@test.org"),
-)
