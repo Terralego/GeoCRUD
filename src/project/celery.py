6fc72c00354f7be43359785740905db43d99a8dc
J-E Castagnede <j.e.castagnede@gmail.com>
2020-10-12 16:22:20 +0200

    use generic broker_url

--- src/project/celery.py
+++ src/project/celery.py
@@ -6,15 +6,9 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.dev')
 
-broker_host = os.getenv('BROKER_HOST', 'broker')
-broker_port = os.getenv('BROKER_PORT', '5672')
-broker_user = os.getenv('BROKER_USER', 'guest')
-broker_password = os.getenv('BROKER_PASSWORD', 'guest')
-
-broker_url = f"amqp://{broker_user}:{broker_password}@{broker_host}:{broker_port}/"
 app = Celery('project')
 app.conf.update(
-    broker_url=broker_url,
+    broker_url=os.getenv('BROKER_URL'),
     task_serializer='json',
     task_always_eager=False,
     accept_content=['json'],  # Ignore other content
