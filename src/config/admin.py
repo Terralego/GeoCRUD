7d1022e42e4105f28256d3f9fc02d065563d933e
J-E Castagnede <j.e.castagnede@gmail.com>
2020-10-30 15:54:46 +0100

    update django-mapbox-baselayers

--- src/config/admin.py
+++ src/config/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from geostore import models as geostore_models
-from mapbox_baselayer.admin import MapBaseLayerAdmin
-from mapbox_baselayer.models import MapBaseLayer
 from terra_geocrud import models as geocrud_models, admin as geocrud_admin
 
 
@@ -10,4 +8,3 @@ admin.site.register(geocrud_models.CrudView, geocrud_admin.CrudViewAdmin)
 admin.site.register(geocrud_models.AttachmentCategory, geocrud_admin.AttachmentCategoryAdmin)
 admin.site.register(geostore_models.Layer, geocrud_admin.CrudLayerAdmin)
 admin.site.register(geostore_models.Feature, geocrud_admin.CrudFeatureAdmin)
-admin.site.register(MapBaseLayer, MapBaseLayerAdmin)
